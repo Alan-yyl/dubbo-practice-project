91f3705bd86fcbbfa2ecfdf5f2368d42e349f7ce
alan <yyl_email@126,com>
2021-05-03 22:17:49 +0800

    创建项目

---
+++ order-service-consumer/src/main/java/org/alan/dubbo/service/impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package org.alan.dubbo.service.impl;
+
+import org.alan.dubbo.bean.UserAddress;
+import org.alan.dubbo.service.OrderService;
+import org.alan.dubbo.service.UserService;
+
+import java.util.List;
+
+/**
+ * @Author: Alan
+ * @Date: 2021/5/3 22:12
+ */
+public class UserServiceImpl implements OrderService{
+    @Override
+    public List<UserAddress> initOrder(String userId) {
+        return null;
+    }
+}
